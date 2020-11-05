6dec226742414effcad3979eeda3ea4a5d5db837
Jason Azevedo <jasonazevedo03@gmail.com>
2020-11-05 15:37:23 +0200

    Implemented setup and close methods for PasswordDB

--- src/Model/PasswordDB.py
+++ src/Model/PasswordDB.py
@@ -2,8 +2,31 @@ import sqlite3
 
 class PasswordDB:
     # conn: sqlite database connection
-    def __init__(self, conn):
-        pass
+    def __init__(self, conn: sqlite3.Connection):
+        self.conn = conn
+        self.cursor = conn.cursor()
+        self.setup_db()
 
     def setup_db(self):
-        pass
+        create_table_query = '''CREATE TABLE IF NOT EXISTS Passwords (
+                    passFor text, 
+                    password text)'''
+
+        user_exists_query = 'SELECT * FROM Passwords WHERE passFor = \'user\''
+
+        create_user_query = '''INSERT INTO Passwords (passFor, password)
+                                 VALUES (?, ?)'''
+
+        self.cursor.execute(create_table_query)
+
+        if self.cursor.execute(user_exists_query).fetchone() == None:
+            self.cursor.execute(create_user_query, ['user', 'pass'])
+
+            print('Account setup \nname: user\npassword: pass\n')
+
+        self.conn.commit()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+        
--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
-from Model.UserDB import UserDB
 from Model.PasswordDB import PasswordDB
 from Interpreter import Interpreter
+import sqlite3
+from os import path
+
+# Setup
+dbPath = path.join(
+    path.dirname(path.realpath(__file__)), 'pwd.db')
+
+db_conn = sqlite3.connect(dbPath)
+passwordDB = PasswordDB(db_conn)
+
+def Auth():
+    pass
+
 
 if __name__ == "__main__":
     # Login
-    print("This is the login phase")
+    Auth()
 
     # Execute action from args or enter interpreter mode
 
     # Exit the program
+    passwordDB.close()
