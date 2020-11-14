494cc9545089eb064ba226ca5ee794f329add136
Jason Azevedo <jasonazevedo03@gmail.com>
2020-11-14 12:33:40 +0200

    Setup encryption for the passwords

--- src/Model/PasswordDB.py
+++ src/Model/PasswordDB.py
@@ -1,4 +1,5 @@
 import sqlite3
+import codecs
 
 
 class PasswordDB:
@@ -10,20 +11,29 @@ class PasswordDB:
 
     def get_all(self):
         query = 'SELECT * FROM Passwords'
-        return self.cursor.execute(query).fetchall()
+        encrypted_entries = self.cursor.execute(query).fetchall()
+        entries = []
+
+        for entry in encrypted_entries:
+            entries.append([entry[0], codecs.decode(entry[1], 'rot-13')])
+
+        return entries
 
     def get(self, name):
         query = 'SELECT * FROM Passwords WHERE passFor = ?'
-        return self.cursor.execute(query, [name]).fetchone()
+        entry = self.cursor.execute(query, [name]).fetchone()
+
+        return [entry[0], codecs.decode(entry[1], 'rot-13')]
 
     def new(self, name, password):
         entry_exist_query = 'SELECT * FROM Passwords WHERE passFor = ?'
         query = 'INSERT INTO Passwords (passFor, password) VALUES (?, ?)'
+        encrypted_pass = codecs.encode(password, 'rot-13')
 
         if len(self.cursor.execute(entry_exist_query, [name]).fetchall()) >= 1:
             return False
 
-        self.cursor.execute(query, [name, password])
+        self.cursor.execute(query, [name, encrypted_pass])
         self.conn.commit()
         return True
 
@@ -32,8 +42,9 @@ class PasswordDB:
                     passFor = ?,
                     password = ?
                     WHERE passFor = ?'''
+        encrypted_pass = codecs.encode(new_pass, 'rot-13')
 
-        self.cursor.execute(query, [new_name, new_pass, name])
+        self.cursor.execute(query, [new_name, encrypted_pass, name])
         self.conn.commit()
 
     def delete(self, name):
@@ -55,7 +66,8 @@ class PasswordDB:
         self.cursor.execute(create_table_query)
 
         if self.cursor.execute(user_exists_query).fetchone() == None:
-            self.cursor.execute(create_user_query, ['user', 'pass'])
+            self.cursor.execute(create_user_query, [
+                                'user', codecs.encode('pass', 'rot-13')])
 
             print('Account setup: \nname: user\npassword: pass\n')
 
